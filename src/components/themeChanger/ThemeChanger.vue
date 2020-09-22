2b38a0003a04f752a7a4f30958c825f6d203c209
Jenuel Oras Ganawed <jenuelganawed936@gmail.com>
2020-09-23 00:19:37 +0800

    update theme

--- src/components/themeChanger/ThemeChanger.vue
+++ src/components/themeChanger/ThemeChanger.vue
@@ -1,6 +1,6 @@
 <template>
      <div class="setting-changer">
-          <v-btn @click="clickButton()" icon :x-large="windowWidth <= 425 ? false: true" class="button" :class="showButton ? 'night' : 'day'">
+          <v-btn @click="clickButton()" icon :x-large="windowWidth <= 425 ? false: true" class="button" :class="showButton ? 'day' : 'night'">
                <v-icon :x-large="windowWidth <= 425 ? false: true" v-if="showButton">{{ mdiWeatherSunny }}</v-icon>
                <v-icon :x-large="windowWidth <= 425 ? false: true" v-else>{{ mdiWeatherNight }}</v-icon>
           </v-btn>
--- src/main.js
+++ src/main.js
@@ -10,7 +10,6 @@ Vue.config.productionTip = false;
 
 new Vue({
   router,
-  // store,
   vuetify,
   render: h => h(App)
 }).$mount('#app')
