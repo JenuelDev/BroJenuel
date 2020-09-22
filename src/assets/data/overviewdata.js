27f87caf94e296eb9e6ba1bf89b6bc6d9384cea1
Jenuel Oras Ganawed <jenuelganawed936@gmail.com>
2020-09-23 00:07:18 +0800

    update fixed blog missing, and edited the overview date

--- src/assets/data/overviewdata.js
+++ src/assets/data/overviewdata.js
@@ -53,19 +53,19 @@ export default {
                     {
                          h3: "UTalk Philippines - Software Developer",
                          current: true,
-                         date: "Year 2019 to Current Date",
+                         date: "September 2019 to Current Date",
                          content: "I work as a full-time software developer in this company. I was able to work on different frameworks and tools like VueJS, AngularJS, CodeIgniter, Lumen, Ruby on Rails, MongoDB, MySQL, and WordPress.",
                          others: ""
                     },
                     {
                          h3: "Dapayan Eatery La Trinidad – Web System Developer",
-                         date: "Year 2018 to 2019",
+                         date: "August 2018 to May 2019",
                          content: 'I participated as an author of the Project Study entitled as Dap-ayan Ordering Management System during the 1rst public research at Kings College of The Philippines on April 6, 2019. I worked mostly on the front end and back end of the system, using mostly PHP programming. <a href="https://media-exp1.licdn.com/media-proxy/ext?w=1024&h=768&f=n&hash=T5nTfH7WcOgNNs3aDH1L6Q3dlCM%3D&ora=1%2CaFBCTXdkRmpGL2lvQUFBPQ%2CxAVta5g-0R6jnhodx1Ey9KGTqAGj6E5DQJHUA3L0CHH05IbfPWjsfs_aK7Dz8EAUKX9TjQAyd--1SDDjFI66eNy6eY0j3cXgd8X5agYUbhl4j3lK6w" target="_blank">View Certificate</a>',
                          others: ""
                     },
                     {
                          h3: "ILearn Development Skills Academy - OJT (Graphic Designer and Programmer)",
-                         date: "Year 2018",
+                         date: "August 2018 to December 2018",
                          content: 'Creating web designs, mostly used is Bootstrap Framework. Helped in programming the back end and front end of system , Video Editing and Photoshop (Infographics, tarpaulins, images)',
                          others: ""
                     }
