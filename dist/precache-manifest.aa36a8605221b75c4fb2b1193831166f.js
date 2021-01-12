87ca7789ed0ff41ce7d1acbdd4418619bd4fffbc
Jenuel Oras Ganawed <jenuelganawed936@gmail.com>
2021-01-12 13:44:31 +0800

    Update Portfolio

---
+++ dist/precache-manifest.aa36a8605221b75c4fb2b1193831166f.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d38a2b58df330c85e0029eecf71d7c26",
+    "url": "/_redirects"
+  },
+  {
+    "revision": "9ead1927b61393cb3780",
+    "url": "/css/about.96e2a1cf.css"
+  },
+  {
+    "revision": "7e819ef96849a2441d02",
+    "url": "/css/app.55e4e690.css"
+  },
+  {
+    "revision": "9e42468c0cfe04d4e58e",
+    "url": "/css/chunk-vendors.69ca7b10.css"
+  },
+  {
+    "revision": "c25d750b26710c1a26de",
+    "url": "/css/codechallenge.aaa67911.css"
+  },
+  {
+    "revision": "17475704e6ee909fda54",
+    "url": "/css/contact.af2ddab9.css"
+  },
+  {
+    "revision": "35f860688ebf6e6ba37f",
+    "url": "/css/experience.9fa6374d.css"
+  },
+  {
+    "revision": "77f81705122e9c0ccf72",
+    "url": "/css/work.30c15160.css"
+  },
+  {
+    "revision": "8eff18f563583d8651a56f4d8a175313",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ead1927b61393cb3780",
+    "url": "/js/about.37f7d378.js"
+  },
+  {
+    "revision": "7e819ef96849a2441d02",
+    "url": "/js/app.fcec27c6.js"
+  },
+  {
+    "revision": "9e42468c0cfe04d4e58e",
+    "url": "/js/chunk-vendors.63945790.js"
+  },
+  {
+    "revision": "c25d750b26710c1a26de",
+    "url": "/js/codechallenge.f4421b72.js"
+  },
+  {
+    "revision": "17475704e6ee909fda54",
+    "url": "/js/contact.bc40cc39.js"
+  },
+  {
+    "revision": "35f860688ebf6e6ba37f",
+    "url": "/js/experience.7c8786d4.js"
+  },
+  {
+    "revision": "77f81705122e9c0ccf72",
+    "url": "/js/work.da3e051c.js"
+  },
+  {
+    "revision": "a3b6bee43f4004cef379034b2b588375",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
