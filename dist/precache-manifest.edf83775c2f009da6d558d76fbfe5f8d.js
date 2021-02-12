ad06e2d4f5e45a3616e444b34585d191642e496c
Jenuel Oras Ganawed <jenuelganawed936@gmail.com>
2021-02-12 23:57:49 +0800

    updated images so that it will look good

--- dist/precache-manifest.edf83775c2f009da6d558d76fbfe5f8d.js
+++ dist/precache-manifest.edf83775c2f009da6d558d76fbfe5f8d.js
@@ -1,15 +1,15 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5da93be32d6e9fdb3de2",
-    "url": "/css/app.33c897e0.css"
+    "revision": "460257e2b3b680ffe725",
+    "url": "/css/app.79a8f4f7.css"
   },
   {
-    "revision": "581e9dff796c76125682149bdea39af9",
+    "revision": "c925f6c1446fa6c36cdc7c045c522811",
     "url": "/index.html"
   },
   {
-    "revision": "5da93be32d6e9fdb3de2",
-    "url": "/js/app.ee45bb5e.js"
+    "revision": "460257e2b3b680ffe725",
+    "url": "/js/app.c45af5ca.js"
   },
   {
     "revision": "344524dd20990cb5a71b",
