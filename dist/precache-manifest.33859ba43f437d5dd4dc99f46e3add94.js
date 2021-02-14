214c600dd0afe518fdb7cc2d661465c702c73328
Jenuel Oras Ganawed <jenuelganawed936@gmail.com>
2021-02-15 01:35:07 +0800

    added contack form in contact page

---
+++ dist/precache-manifest.33859ba43f437d5dd4dc99f46e3add94.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a35f7f99eebb78e07dbf",
+    "url": "/css/app.f03a4456.css"
+  },
+  {
+    "revision": "3eb66d4f4f31c946a454",
+    "url": "/css/chunk-820603d8.62afef1d.css"
+  },
+  {
+    "revision": "aa9eb684cd03f2f7b13e9c97ca08c6d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a35f7f99eebb78e07dbf",
+    "url": "/js/app.e3866809.js"
+  },
+  {
+    "revision": "3eb66d4f4f31c946a454",
+    "url": "/js/chunk-820603d8.92991895.js"
+  },
+  {
+    "revision": "56a6dc9da4d0bb16762f",
+    "url": "/js/chunk-vendors.9989a8e2.js"
+  },
+  {
+    "revision": "a3b6bee43f4004cef379034b2b588375",
+    "url": "/manifest.json"
+  }
+]);
