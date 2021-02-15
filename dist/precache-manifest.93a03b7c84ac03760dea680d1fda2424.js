2b2aaf210922cb93d94865eb7793b45643dbc8d8
Jenuel Oras Ganawed <jenuelganawed936@gmail.com>
2021-02-15 12:07:23 +0800

    updated so that when new content arrive it will update the page

--- dist/precache-manifest.93a03b7c84ac03760dea680d1fda2424.js
+++ dist/precache-manifest.93a03b7c84ac03760dea680d1fda2424.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a35f7f99eebb78e07dbf",
+    "revision": "62e029871f2640be49f0",
     "url": "/css/app.f03a4456.css"
   },
   {
-    "revision": "3eb66d4f4f31c946a454",
+    "revision": "31f48c38e7895363137a",
     "url": "/css/chunk-820603d8.62afef1d.css"
   },
   {
-    "revision": "aa9eb684cd03f2f7b13e9c97ca08c6d0",
+    "revision": "4839253fd3a91400fc613dcd1999950d",
     "url": "/index.html"
   },
   {
-    "revision": "a35f7f99eebb78e07dbf",
-    "url": "/js/app.e3866809.js"
+    "revision": "62e029871f2640be49f0",
+    "url": "/js/app.9d8da7be.js"
   },
   {
-    "revision": "3eb66d4f4f31c946a454",
-    "url": "/js/chunk-820603d8.92991895.js"
+    "revision": "31f48c38e7895363137a",
+    "url": "/js/chunk-820603d8.303de728.js"
   },
   {
     "revision": "56a6dc9da4d0bb16762f",
