e581aac5ada5c87e0b951bdac867b726a8dc89ac
Jenuel Ganawed <jenuelganawed936@gmail.com>
2023-01-10 13:54:10 +0800

    Updates

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,39 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+    plugins: [
+        vue(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            injectRegister: 'auto',
+            workbox: {
+                cleanupOutdatedCaches: true,
+                globPatterns: ['**/*.{js,css,html,ico,png,svg,json,vue,txt,woff2}'],
+            },
+            manifest: {
+                name: 'BroJenuel - Jenuel Ganawed',
+                short_name: 'BroJenuel',
+                description:
+                    'Jenuel Ganawed(BroJenuel/Bro Jenuel) is a software/web developer specializing in creating (and sometimes designing) exceptional websites, applications, and everything in between.',
+                theme_color: '#64ffdb',
+                background_color: '#0a192fd9',
+                display: 'standalone',
+                icons: [
+                    {
+                        src: 'icons/android-chrome-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'icons/android-chrome-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                ],
+            },
+        }),
+    ],
+});
