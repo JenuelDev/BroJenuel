4216a7fbd05ffe226cd77118ccbdcbff8d8d9dcd
Jenuel Ganawed <jenuelganawed936@gmail.com>
2023-01-14 22:53:43 +0800

    Created A MetaTag function in composable

--- nuxt3/pages/index.vue
+++ nuxt3/pages/index.vue
@@ -1,4 +1,18 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const route = useRoute();
+const { setMeta } = useMeta();
+
+useHead(
+    setMeta({
+        title: "BroJenuel - Jenuel Oras Ganawed",
+        description:
+            "Jenuel Ganawed(BroJenuel/Bro Jenuel) is a software/web developer specializing in creating (and sometimes designing) exceptional websites, applications, and everything in between.",
+        path: route.path,
+        keywords: ["brojenuel", "Jenuel", "Jenuel Ganawed", "bro jenuel", "web developer", "software developer"],
+        image: "https://brojenuel.com/img/profileImage.ac3f181b.webp",
+    })
+);
+</script>
 <template>
     <section class="h-100vh flex items-center justify-center">
         <div class="text-right">
