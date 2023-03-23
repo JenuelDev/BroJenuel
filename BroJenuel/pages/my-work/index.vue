47fd83cd1aa13b3fc2714ee51b30c9347a82f441
Jenuel Ganawed <jenuelganawed936@gmail.com>
2023-03-23 19:33:23 +0800

    Update

--- BroJenuel/pages/my-work/index.vue
+++ BroJenuel/pages/my-work/index.vue
@@ -13,8 +13,16 @@ const WORKS = [
         description: "A app for studying the bible for desktop. Currently supports windows only.",
         techs: ["electron", "electron.builder", "Vue3", "windiCss"],
         links: [
-            { link: "https://github.com/Bible-Projects/believers-sword-app", icon: "ant-design:github-filled", tooltip: "Open in Github" },
-            { link: "https://believers-sword.brojenuel.com/", icon: "mingcute:external-link-line", tooltip: "Open Link" },
+            {
+                link: "https://github.com/Bible-Projects/believers-sword-app",
+                icon: "ant-design:github-filled",
+                tooltip: "Open in Github",
+            },
+            {
+                link: "https://believers-sword.brojenuel.com/",
+                icon: "mingcute:external-link-line",
+                tooltip: "Open Link",
+            },
         ],
         imageLink: "https://believers-sword.brojenuel.com/",
         img: "believers-sword.webp",
@@ -22,6 +30,28 @@ const WORKS = [
         date: "2023-02-17",
         logo: "https://believers-sword.brojenuel.com/_nuxt/believers-sword.7fcefc1c.svg",
         url: "/blog/Believers-Sword",
+        url_new_tab: false,
+    },
+    {
+        overline: "Documentation Project",
+        title: "Laravel MongoDB Docs",
+        description: "Documentation how you can integrate Laravel with MongoDB.",
+        techs: ["vuepress", "vue"],
+        links: [
+            {
+                link: "https://github.com/BroJenuel-Youtube/laravel-mongodb",
+                icon: "ant-design:github-filled",
+                tooltip: "Open in Github",
+            },
+            { link: "https://laravel-mongo-docs.ml", icon: "mingcute:external-link-line", tooltip: "Open Link" },
+        ],
+        imageLink: "https://laravel-mongo-docs.ml",
+        img: "laravel-mongo-db.webp",
+        tags: ["website", "documentation"],
+        date: "2023-02-17",
+        logo: "https://laravel-mongo-docs.ml/assets/img/logo.png",
+        url: "https:://laravel-mongodb.vercel.app",
+        url_new_tab: true,
     },
 ];
 
@@ -53,11 +83,22 @@ defineOgImageStatic({
                         My Work
                     </div>
                     <div class="pt-5">
-                        <div class="indent-md">Showing are personal projects that I made during my spare/free time. I don't add projects from my Work/Jobs, only if allowed.</div>
+                        <div class="indent-md">
+                            Showing are personal projects that I made during my spare/free time. I don't add projects
+                            from my Work/Jobs, only if allowed.
+                        </div>
                     </div>
                     <div class="grid grid-cols-1 gap-3">
-                        <NuxtLink v-for="(work, i) in WORKS" :key="i" :to="work.url" class="p-10px rounded-md cursor-pointer group">
-                            <div class="border-5 border-[var(--gray-lightest)] flex justify-center items-center h-150px overflow-hidden rounded-lg relative">
+                        <NuxtLink
+                            v-for="(work, i) in WORKS"
+                            :key="i"
+                            :to="work.url"
+                            class="p-10px rounded-md cursor-pointer group"
+                            :target="work.url_new_tab ? '_blank' : '_self'"
+                        >
+                            <div
+                                class="border-5 border-[var(--gray-lightest)] flex justify-center items-center h-150px overflow-hidden rounded-lg relative"
+                            >
                                 <img
                                     :src="`/img/work/${work.img}`"
                                     class="absolute w-full transform scale-100 group-hover:scale-transform-120 transition-all"
@@ -71,13 +112,22 @@ defineOgImageStatic({
                                     <img class="w-30px" :src="work.logo" />
                                 </div>
                                 <div class="w-[80%]">
-                                    <div class="font-700 group-hover:text-[var(--primary)] text-size-20px">{{ work.title }}</div>
+                                    <div class="font-700 group-hover:text-[var(--primary)] text-size-20px">
+                                        {{ work.title }}
+                                    </div>
                                     <div>{{ work.description }}</div>
                                 </div>
                             </div>
                         </NuxtLink>
-                        <NuxtLink v-for="(work, i) in myWork" :key="i" :href="work._path" class="p-10px rounded-md cursor-pointer group">
-                            <div class="border-5 border-[var(--gray-lightest)] flex justify-center items-center h-150px overflow-hidden rounded-lg relative">
+                        <NuxtLink
+                            v-for="(work, i) in myWork"
+                            :key="i"
+                            :href="work._path"
+                            class="p-10px rounded-md cursor-pointer group"
+                        >
+                            <div
+                                class="border-5 border-[var(--gray-lightest)] flex justify-center items-center h-150px overflow-hidden rounded-lg relative"
+                            >
                                 <img
                                     :src="`/img/work/${work.img}`"
                                     class="absolute w-full transform scale-100 group-hover:scale-transform-120 transition-all"
@@ -91,7 +141,9 @@ defineOgImageStatic({
                                     <img class="w-30px" :src="work.logo" />
                                 </div>
                                 <div class="w-[80%]">
-                                    <div class="font-700 group-hover:text-[var(--primary)] text-size-20px">{{ work.title }}</div>
+                                    <div class="font-700 group-hover:text-[var(--primary)] text-size-20px">
+                                        {{ work.title }}
+                                    </div>
                                     <div>{{ work.description }}</div>
                                 </div>
                             </div>
@@ -116,7 +168,12 @@ defineOgImageStatic({
                                 </div>
                                 <div class="flex gap-2 text-size-20px pt-2">
                                     <template v-for="link in codeChallenge.links">
-                                        <a :href="link.link" target="_blank" :title="link.tooltip" class="hover:text-[var(--primary)]">
+                                        <a
+                                            :href="link.link"
+                                            target="_blank"
+                                            :title="link.tooltip"
+                                            class="hover:text-[var(--primary)]"
+                                        >
                                             <Icon :name="link.icon" />
                                         </a>
                                     </template>
